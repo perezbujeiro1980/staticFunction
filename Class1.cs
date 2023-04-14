9bb86afb68f226b3dee1a5d24f4dceb12fd5ead8
githubawaq <97857484+perezbujeiro1980@users.noreply.github.com>
2023-04-14 16:37:56 -0300

    ok

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class MyClass
+    {
+        public static void Method1()
+        {
+            // Method1
+        }
+
+        public static void Method2()
+        {
+            
+        }
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            Console.Write("El número máximo es {0}", getMax());
+            Console.ReadKey();
+        }
+
+       static double getMax()
+        {
+            int[] nums = { 1, 2, 5, 4, 0, 3,9 };
+            Double max_num = Double.NegativeInfinity;
+
+            foreach (var n in nums)
+            {
+                if (n > max_num)
+                    max_num = n;
+            }
+
+            return max_num;
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
